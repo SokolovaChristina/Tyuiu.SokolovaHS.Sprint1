80592d94f4371db5643893091d42f6512f575e2b
SokolovaChristina <Tyuiu.SokolovaHS@outlook.com>
2025-09-23 22:27:14 +0500

    Task7

---
+++ Tyuiu.SokolovaHS.Sprint1.Task7.V26.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.SokolovaHS.Sprint1.Task7.V26.Lib
+{
+    public class DataService : ISprint1Task7V26
+    {
+        public double Calculate(double x, double y)
+        {
+          double part1 = Math.Pow(Math.Sin(x), 2) + (y / (y + 1));
+          double part2 = (x * y) - (12.0 / (34 + Math.Pow(x, 2)));
+          double z = part1 - part2;
+          return Math.Round(z, 3);
+
+        }
+    }
+}
---
+++ Tyuiu.SokolovaHS.Sprint1.Task7.V26.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SokolovaHS.Sprint1.Task7.V26.Lib;
+
+namespace Tyuiu.SokolovaHS.Sprint1.Task7.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 3;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(-3.696, res);
+        }
+    }
+}
