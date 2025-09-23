3790269be6e3afd1bd8a11d8a51bb3377714529d
SokolovaChristina <Tyuiu.SokolovaHS@outlook.com>
2025-09-23 23:40:20 +0500

    Task7

--- Tyuiu.SokolovaHS.Sprint1.Task7.V26/Program.cs
+++ Tyuiu.SokolovaHS.Sprint1.Task7.V26/Program.cs
@@ -15,7 +15,7 @@ namespace Tyuiu.SokolovaHS.Sprint1.Task7.V26
             Console.Title = "Спринт #1 | Выполнила: Соколова Х. С. | ПКТБ-25-1";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #1                                                               *");
-            Console.WriteLine("* Тема: Class Math                                                        *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
             Console.WriteLine("* Задание #7                                                              *");
             Console.WriteLine("* Вариант #26                                                             *");
             Console.WriteLine("* Выполнила: Соколова Христина Сергеевна | ПКТБ-25-1                      *");
